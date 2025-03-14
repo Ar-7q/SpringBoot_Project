153af9ee636db458385af21175dbe807a148fcbd
Ar-7q <arpitrai927@gmail.com>
2025-03-15 03:09:00 +0530

    simply solving the application properties to the resources

--- app/src/main/java/org/example/bean/UserConfig.java
+++ app/src/main/java/org/example/bean/UserConfig.java
@@ -1,16 +1,22 @@
 package org.example.bean;
 
-class UserConfig {
-
+public class UserConfig {
     private String name;
     private String className;
 
-    public void setname(final String name){
-        this.name=name;
-    }
-    public void setClassName(final String className){
-        this.className=className;
+    public String getName() {
+        return name;
     }
 
-    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
 }
